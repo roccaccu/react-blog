6019dcc4dc5705bf71ad8c54b2f46b913ec7d88f
roccaccu <naimadzhun@gmail.com>
2022-07-22 17:44:21 +0800

    fifth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { useEffect, useMemo, useState } from 'react'
 import Header from './components/Header/Header'
-import Modal from './components/Modal/Modal'
 import PostFilter from './components/PostFilter/PostFilter'
 import PostForm from './components/PostForm/PostForm'
 import PostList from './components/PostList/PostList'
 import Button from './components/UI/Button/Button'
+import Modal from './components/UI/Modal/Modal'
+import { useEffect, useState } from 'react'
+import { usePosts } from './hooks/usePosts'
+import PostService from './api/postService'
+import Spinner from './components/Loader/Loader'
 
 import './style/App.css'
 
@@ -12,35 +15,29 @@ const App = () => {
 	const [posts, setPosts] = useState([])
 	const [filter, setFilter] = useState({ sort: '', query: '' })
 	const [activeModal, setActiveModal] = useState(false)
+	const [isLoading, setIsLoading] = useState(false)
+	const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query)
 
-	const sortedPost = useMemo(() => {
-		if (filter.sort) {
-			return [...posts].sort((a, b) =>
-				a[filter.sort].localeCompare(b[filter.sort])
-			)
-		}
-		return posts
-	}, [filter.sort, posts])
+	useEffect(() => {
+		getAllPosts()
+	}, [])
 
-	const sortedAndSearchPosts = useMemo(() => {
-		return sortedPost.filter(post =>
-			post.title.toLowerCase().includes(filter.query.toLowerCase())
-		)
-	}, [filter.query, sortedPost])
+	async function getAllPosts() {
+		const data = await PostService.getAll()
+		setPosts(data)
+		setIsLoading(true)
+	}
 
 	const createPost = post => {
 		post.id = Date.now()
 		setPosts([...posts, post])
+		setActiveModal(false)
 	}
 
 	const deletedPost = post => {
 		setPosts(posts.filter(p => p.id !== post.id))
 	}
 
-	// const sortPost = sort => {
-	//   setSelectedSort(sort)
-	//   setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
-	// }
 	return (
 		<div>
 			<Header />
@@ -55,19 +52,23 @@ const App = () => {
 					<PostForm create={createPost} />
 				</Modal>
 				<PostFilter filter={filter} setFilter={setFilter} />
-				{sortedAndSearchPosts.length > 0 ? (
-					<PostList posts={sortedAndSearchPosts} deleted={deletedPost} />
+				{isLoading ? (
+					sortedAndSearchPosts.length > 0 ? (
+						<PostList posts={sortedAndSearchPosts} deleted={deletedPost} />
+					) : (
+						<h2
+							style={{
+								textAlign: 'center',
+								fontWeight: 900,
+								fontSize: '30px',
+								color: '#1e212c',
+							}}
+						>
+							Список пуст :)
+						</h2>
+					)
 				) : (
-					<h2
-						style={{
-							textAlign: 'center',
-							fontWeight: 900,
-							fontSize: '30px',
-							color: '#1e212c',
-						}}
-					>
-						Список пуст :)
-					</h2>
+					<Spinner />
 				)}
 			</div>
 		</div>
